ca7638e4a5ad847df841cde10922dcc5f5b9f213
wtuyp <wtuyp@163.com>
2015-08-18 17:35:25 +0800

    The Swift Programming Language 学习
    学习:
    基础部分
    基本运算符
    字符串和字符

--- SwiftOnTheWay.playground/Contents.swift
+++ SwiftOnTheWay.playground/Contents.swift
@@ -249,3 +249,97 @@ enum Suit {
 let hearts = Suit.Hearts
 let heartsDescription = hearts.simpleDescription()
 let heartsColor = hearts.color()
+
+/**
+    The Swift Programming Language 学习
+       http://wiki.jikexueyuan.com/project/swift/ 
+*/
+
+//整数范围
+let minValue = UInt8.min  // minValue 为 0，是 UInt8 类型
+let maxValue = UInt8.max  // maxValue 为 255，是 UInt8 类型
+
+let pi = 3.14159
+// pi 会被推测为 Double 类型
+
+let anotherPi = 3 + 0.14159
+// anotherPi 会被推测为 Double 类型
+
+let decimalInteger = 17
+let binaryInteger = 0b10001       // 二进制的17
+let octalInteger = 0o21           // 八进制的17
+let hexadecimalInteger = 0x11     // 十六进制的17
+
+//数值类字面量可以包括额外的格式来增强可读性。整数和浮点数都可以添加额外的零并且包含下划线，并不会影响字面量：
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+
+
+//Swift 的nil和 Objective-C 中的nil并不一样。在 Objective-C 中，nil是一个指向不存在对象的指针。在 Swift 中，nil不是指针——它是一个确定的值，用来表示值缺失。任何类型的可选状态都可以被设置为nil，不只是对象类型。
+
+//隐式解析可选类型（implicitly unwrapped optionals）。把想要用作可选的类型的后面的问号（String?）改成感叹号（String!）来声明一个隐式解析可选类型。
+let possibleString: String? = "An optional string."
+let forcedString: String = possibleString! // 需要惊叹号来获取值
+
+let assumedString: String! = "An implicitly unwrapped optional string."
+let implicitString: String = assumedString  // 不需要感叹号
+
+
+//不同于 C 语言和 Objective-C，Swift 中是可以对浮点数进行求余的。
+8 % 2.5 // 等于 0.5
+
+//一元负号运算符
+let three = 3
+let minusThree = -three       // minusThree 等于 -3
+let plusThree = -minusThree   // plusThree 等于 3, 或 "负负3"
+
+/*空合运算符(Nil Coalescing Operator)
+  空合运算符(a ?? b)将对可选类型a进行空判断，如果a包含一个值就进行解封，否则就返回一个默认值b.这个运算符有两个条件:
+  表达式a必须是Optional类型
+  默认值b的类型必须要和a存储值的类型保持一致
+  a != nil ? a! : b  */
+let defaultColorName = "red"
+var userDefinedColorName:String?   //默认值为nil
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+//userDefinedColorName的值为空 ，所以colorNameToUse的值为`red`
+userDefinedColorName = "green"
+colorNameToUse = userDefinedColorName ?? defaultColorName
+//userDefinedColorName非空，因此colorNameToUsede的值为绿色
+
+//插值字符串中写在括号中的表达式不能包含非转义双引号 (") 和反斜杠 (\)，并且不能包含回车或换行符。
+
+//字符串字面量可以包含以下特殊字符：
+//
+//转义字符\0(空字符)、\\(反斜线)、\t(水平制表符)、\n(换行符)、\r(回车符)、\"(双引号)、\'(单引号)。
+//Unicode 标量，写成\u{n}(u为小写)，其中n为任意一到八位十六进制数且可用的 Unicode 位码。
+let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+// "Imageination is more important than knowledge" - Enistein
+let dollarSign = "\u{24}"             // $, Unicode 标量 U+0024
+let blackHeart = "\u{2665}"           // ♥, Unicode 标量 U+2665
+let sparklingHeart = "\u{1F496}"      // 💖, Unicode 标量 U+1F496
+
+let eAcute: Character = "\u{E9}"                         // é
+let combinedEAcute: Character = "\u{65}\u{301}"          // e 后面加上  ́
+// eAcute 是 é, combinedEAcute 是 é
+
+let precomposed: Character = "\u{D55C}"                  // 한
+let decomposed: Character = "\u{1112}\u{1161}\u{11AB}"   // ᄒ, ᅡ, ᆫ
+// precomposed 是 한, decomposed 是 한
+
+let greeting1 = "Guten Tag"
+println(greeting1.startIndex)
+// 0
+println(greeting1.endIndex)
+// 9
+greeting[greeting1.startIndex]
+// G
+greeting[greeting1.endIndex.predecessor()]
+// g
+greeting[greeting1.startIndex.successor()]
+// u
+let index = advance(greeting1.startIndex, 7)
+greeting1[index]
+// a
+greeting1[greeting1.endIndex] // 错误
+greeting1.endIndex.successor() // 错误
